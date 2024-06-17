bd618540211e63a0e12f85bf81eece26008c6c9a
REHXZ <rehxxz@gmail.com>
2024-06-17 08:58:42 +0800

    updated

--- test/test.js
+++ test/test.js
@@ -1,36 +1,42 @@
-const url = 'https://api.sandbox.hit-pay.com/v1/payment-requests';  // Replace with your actual URL
+async function CallUrl(a, b) {
+    const headers = {
+        'X-BUSINESS-API-KEY': '4867e54baa78dfc7423ef254d08e0994592eef664afffad81fe266cc1486cbe7',
+        'X-Requested-With': 'XMLHttpRequest'
+    };
 
-const headers = {
-    'X-BUSINESS-API-KEY': '4867e54baa78dfc7423ef254d08e0994592eef664afffad81fe266cc1486cbe7',
-    'X-Requested-With': 'XMLHttpRequest'
-};
+    const data = new URLSearchParams({
+        amount: a,
+        currency: 'SGD',
+        purpose: b,
+        reference_number: b,
+        'payment_methods[]': ["paynow_online"],
+        email: 'example@example.com',
+        redirect_url: "https://minds-app-updated-socceeer.apps.hackathon.cnasg.dellcsc.com"
+    });
 
-const data = new URLSearchParams({
-    amount: '15.00',
-    currency: 'SGD',
-    purpose: 'S1234567J',
-    reference_number: 'S1234567J',
-    'payment_methods[]': ["paynow_online"],
-    email: 'example@example.com'
-});
+    const apiEndpoint = 'https://api.sandbox.hit-pay.com/v1/payment-requests';
 
-fetch(url, {
-    method: 'POST',
-    headers: headers,
-    body: data
-})
-.then(response => {
-    if (!response.ok) {
-        // Log the response status and status text
-        return response.text().then(text => {
-            throw new Error(`Network response was not ok: ${response.status} - ${response.statusText} - ${text}`);
+    try {
+        const response = await fetch(apiEndpoint, {
+            method: 'POST',
+            headers: headers,
+            body: data
         });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        return result.url;
+    } catch (error) {
+        console.error('Error:', error);
     }
-    return response.json();
-})
-.then(data => {
-    console.log(data);
-})
-.catch(error => {
-    console.error('There has been a problem with your fetch operation:', error);
-});
+}
+
+async function main() {
+    const x = await CallUrl('10.0', 'S1234567J');
+    console.log(x);
+}
+
+main()
---
+++ test/test2.js
@@ -0,0 +1,8 @@
+import CallUrl from test
+
+async function main() {
+    const x = await CallUrl('10.0', 'S1234567J');
+    console.log(x);
+}
+
+main()
